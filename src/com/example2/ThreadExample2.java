539352a9852e6a2a9e762c2977b83923b5386b34
Manoj M <manumadhu1425@gmail.com>
2024-01-01 13:10:44 +0530

    Thread inheritance

---
+++ src/com/example2/ThreadExample2.java
@@ -0,0 +1,17 @@
+package com.example2;
+
+public class ThreadExample2 {
+	public static void main(String[] args) {
+		Thread thread = new NewThread();
+		thread.start();
+	}
+	
+	private static class NewThread extends Thread{
+		
+		@Override
+		public void run() {
+			System.out.println("Hello from "+ this.getName());
+		}
+	}
+}
+
